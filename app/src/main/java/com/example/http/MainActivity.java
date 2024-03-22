34b06fed43d1c3ce4abc0cef9788e94037282bc7
Nikkikorzh <nikkikbloger@gmail.com>
2024-03-22 19:07:33 +0200

    Url

--- app/src/main/java/com/example/http/MainActivity.java
+++ app/src/main/java/com/example/http/MainActivity.java
@@ -10,10 +10,14 @@ import android.widget.Button;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +26,12 @@ public class MainActivity extends AppCompatActivity {
     OkHttpClient client;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.getButton);
         String urlString = "https://publicobject.com/helloworld.txt";
         client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -33,35 +39,37 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
 
-        button = findViewById(R.id.button2);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thread newThread = new Thread() {
+                client.newCall(request).enqueue(new Callback() {
                     @Override
-                    public void run() {
-                        try (Response response = client.newCall(request).execute()) {
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        try (ResponseBody responseBody = response.body()) {
                             if (!response.isSuccessful()) {
                                 throw new IOException("Запрос к серверу не был успешен: " +
                                         response.code() + " " + response.message());
                             }
-                            // пример получения конкретного заголовка ответа
-                            Log.d("test",response.toString());
-                            // вывод тела ответа
-                            Log.d("test",response.body().string());
-                        } catch (IOException e) {
-                            Log.d("test","Ошибка подключения: " + e);
-                        }
 
+                            // пример получения всех заголовков ответа
+                            Headers responseHeaders = response.headers();
+                            for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                                // вывод заголовков
+                                Log.d("test",responseHeaders.name(i) + ": "
+                                        + responseHeaders.value(i));
+                            }
+                            // вывод тела ответа
+                            Log.d("test",responseBody.string());
+                        }
                     }
-                };
-                newThread.start();
+                });
             }
         });
-
-
-
-
-
     }
 }
